035a70d70687f3de16f043b46a4bf1da54c26e4b
augustkeller <guskeller@gmail.com>
2026-01-08 12:26:03 -0600

    useState firs step

--- solo-project/src/components/ToDoList.jsx
+++ solo-project/src/components/ToDoList.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
+
 export default function ToDoList() {
+    const [isChecked, setIsChecked] = useState(false);
+
+    function HandleChange(event) {
+        const checked = event.target.checked;
+        setIsChecked(checked);
+    }
+
     return (
         <div className="category-selector">
-            <input type="checkbox" value="Fart" /> Fart<br />
-            <input type="checkbox" value="Butt" /> Butt<br />
-            <input type="checkbox" value="Sing" /> Sing<br />
-            <input type="checkbox" value="Fart Again" /> Fart Again<br />
-            <input type="checkbox" value="Watch Moobie" /> Watch Moobie<br />
-            <input type="checkbox" value="Cody" /> Cody<br />
-            <input type="checkbox" value="Practice" /> Practice<br />
-            <input type="checkbox" value="Gym" /> Gym<br />
-            <input type="checkbox" value="MTG" /> MTG<br />
-            <input type="checkbox" value="Relax" /> Relax<br />
-            <input type="checkbox" value="Pet Cats" /> Pet Cats<br />
+            <input type="checkbox" value="Fart" onChange={HandleChange} /> Fart {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Butt" onChange={HandleChange} /> Butt {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Sing" onChange={HandleChange} /> Sing {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Fart Again" onChange={HandleChange} /> Fart Again {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Watch Moobie" onChange={HandleChange} /> Watch Moobie {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Cody" onChange={HandleChange} /> Cody {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Practice" onChange={HandleChange} /> Practice {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Gym" onChange={HandleChange} /> Gym {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="MTG" onChange={HandleChange} /> MTG {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Relax" onChange={HandleChange} /> Relax {isChecked && <span>Time</span>} <br />
+            <input type="checkbox" value="Pet Cats" onChange={HandleChange} /> Pet Cats {isChecked && <span>Time</span>} <br />
         </div>
     );
 }
