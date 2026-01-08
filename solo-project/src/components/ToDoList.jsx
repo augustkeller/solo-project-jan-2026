448ab0e4c76ea714701a711aa1d005f291d31eda
augustkeller <guskeller@gmail.com>
2026-01-08 12:18:46 -0600

    to do list update

--- solo-project/src/components/ToDoList.jsx
+++ solo-project/src/components/ToDoList.jsx
@@ -1,11 +1,17 @@
 export default function ToDoList() {
-   return (
-       <div>
-            <ul>
-                <li>Learn React</li>
-                <li>Build a project</li>
-                <li>Read documentation</li>
-            </ul>
-       </div>
-   );
+    return (
+        <div className="category-selector">
+            <input type="checkbox" value="Fart" /> Fart<br />
+            <input type="checkbox" value="Butt" /> Butt<br />
+            <input type="checkbox" value="Sing" /> Sing<br />
+            <input type="checkbox" value="Fart Again" /> Fart Again<br />
+            <input type="checkbox" value="Watch Moobie" /> Watch Moobie<br />
+            <input type="checkbox" value="Cody" /> Cody<br />
+            <input type="checkbox" value="Practice" /> Practice<br />
+            <input type="checkbox" value="Gym" /> Gym<br />
+            <input type="checkbox" value="MTG" /> MTG<br />
+            <input type="checkbox" value="Relax" /> Relax<br />
+            <input type="checkbox" value="Pet Cats" /> Pet Cats<br />
+        </div>
+    );
 }
